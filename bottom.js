7e93caeaf6daab3ef3ad86d427908684b98c5849
vijay24029 <vijaykumar24029@gmail.com>
2023-11-17 18:28:47 +0530

    school
    school

---
+++ bottom.js
@@ -0,0 +1,71 @@
+document.write(`<div class="container-fluid cng  p-4">
+<div class="row mb-5">
+  <div class="col-1"></div>
+  <div class="col-lg-6 col-md-12 col-sm-12"> 
+    <h3 class="mt-5">About Gian Tarang</h3>
+    <p class="mt-4 ">GIAN Tarang Pre School in SAS Nagar Mohali provides a stimulating environment for your child's early years. Our experienced team offers personalized attention, innovative curriculum, and fun activities to help your child learn and grow. Enroll today!         </p>
+    <a href="#" class="text-decoration-none text-secondary mt-5 "><i class="fa-solid fa-phone pe-1"></i>+917888945643</a>
+    <p class="text-secondary mt-2 "><i class="fa-solid fa-house pe-2"></i>SAS Nagar Mohali , Punjab</p>
+    <a href="#" class="text-decoration-none text-secondary ">https://giantarangpreschool.com/</a></p>
+
+
+
+    <h1>
+      <i class="fa-brands fa-facebook-f icon1 " id="ic1"></i>
+      <i class="fa-brands fa-instagram icon1" id="ic2"></i>
+      <i class="fa-brands fa-square-youtube icon1" id="ic3"></i>
+      <i class="fa-brands fa-whatsapp icon1" id="ic4"></i>
+      <i class="fa-brands fa-paypal icon1" id="ic5"></i>
+    </h1>
+
+
+  </div>
+  <div class="col-lg-4 col-md-12 col-sm-12">
+    <h3 class="mb-5 mt-5">Recent Blog Post</h3>
+    <div class="row foot1 ">
+
+    
+      <div class="col-lg-4 col-md-4 col-sm-12 mb-4 " ><img src="school pro img/event1684933062.png" alt="" width="150" class=""> </div>
+      <div class="col-8 mb-4"> 
+        <a href="#" class="text-decoration-none text-dark  fw-bold"> the Benefits Of Play-Based Learning In Pre School: Nurturing....</a>
+        <p class="text-secondary">10-05-2023</p></div></div>
+     
+
+    
+      
+      
+    <div class="row foot1">
+      <div class="col-lg-4 col-md-4 col-sm-12 mt-4 mb-4"><img src="school pro img/event1684498199.jpeg" alt="" width="150" class=""> </div>
+    <div class="col-8 mt-4 mb-4"> 
+      <a href="#" class="text-decoration-none text-dark fw-bold ">Play Is An Innate Part Of Childhood.</a>
+     
+      <p class="text-secondary">25-09/2023</p></div></div> 
+
+
+    <div class="row foot1">
+      <div class="col-lg-4 col-md-4 col-sm-12 mt-4 mb-4"><img src="school pro img/event1684933062.png" alt="" width="150" class=""> </div>
+    <div class="col-8 mt-4 mb-4"> 
+      <a href="#" class="text-decoration-none text-dark  fw-bold">Creating a Safe and Stimulating Environment Your child At Gian...</a>
+      <p class="text-secondary">20-12-2022</p></div></div> 
+   
+
+  </div>
+  <div class="col-1"></div>
+</div>
+</div>
+
+
+<div class="container-fluid  bg-secondary p-lg-4 p-md-3 p-sm-2 mb-5">
+<div class="row">
+  <p class="text-center text-dark mt-3  "> &copy;  2023 All Rights Reserved . Design by <span class="text-dark fw-bold">Vijay Patel</span></p>
+</div>
+</div>
+
+
+<div class="container-fluid bg-primary  p-3 mpt fixed-bottom">
+<div class="row">
+  <marquee behavior="" direction="" class="text-light mq ">TAEKWONDO CLASSES ADMISSION OPEN !!! For 2023 - 2024 | For Your Child a Better Future</marquee>
+</div>
+</div>
+
+<script src="bootstrap.bundle.min.js"></script>`)
---
+++ head.js
@@ -0,0 +1,69 @@
+document.write(`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gian Tarang</title>
+  <link rel="shortcut icon" href="school pro img/logo.png" type="image/x-icon">
+  <link rel="stylesheet" href="bootstrap.min.css">
+  <link rel="stylesheet" href="new.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+</head>
+<body>
+    <!-- navbar -->
+   
+    <div class="container-fluid sticky-top bg-light">
+        <nav class="navbar navbar-expand-lg   navbar-light side2 "  >
+          <div class="container-fluid">
+            <a class="navbar-brand ms-lg-5 ms-md-2 ms-1 ps-lg-5" href="#"><img src="school pro img/logo.png" alt="" width="75px" ></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+              aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav nav ms-auto">
+                <li class="nav-item mt">
+      
+                  <a class="nav-link text-danger fw-bold me-4 cal" aria-current="page" href="index.html" ><i class="fa-solid fa-house me-2"></i>Home</a>
+                </li>
+                <li class="nav-item mt">
+                  <a class="nav-link fw-bold me-4 text-secondary" href="about.html" ><i class="fa-regular fa-address-card  me-2"></i>ABOUT US</a>
+                </li>
+                <li class="nav-item dropdown mt">
+                  <a class="nav-link dropdown-toggle fw-bold me-3 text-primary" href="#" id="navbarDropdownMenuLink" role="button"
+                    data-bs-toggle="dropdown" aria-expanded="false" ><i class="fa-solid fa-book  me-2"></i>ACADEMICS</a>
+                  <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <li><a class="dropdown-item" href="scholastic.html">Scholastic</a></li>
+                    <li><a class="dropdown-item" href="co.html">Co-Scholastic</a></li>
+                    <li><a class="dropdown-item" href="activities.html">Activities</a></li>
+                  </ul>
+                </li>
+                <li class="nav-item dropdown mt">
+                  <a class="nav-link dropdown-toggle fw-bold me-4 text-warning" href="#" id="navbarDropdownMenuLink" role="button"
+                    data-bs-toggle="dropdown" aria-expanded="false"><i class="fa-brands fa-servicestack  me-2"></i>
+                    SERVICES
+                  </a>
+                  <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <li><a class="dropdown-item" href="day.html">Day Care</a></li> 
+                    <li><a class="dropdown-item" href="summer.html">Summer Camp</a></li> 
+                    <li><a class="dropdown-item" href="workshop.html">Workshop</a></li>
+                    <li><a class="dropdown-item" href="online.html">Online/Ofline Tution</a></li>
+                  </ul>
+                </li>
+                <li class="nav-item mt">
+                  <a class="nav-link fw-bold me-4 text-info" aria-current="page" href="gallery.html"><i class="fa-brands fa-envira  me-2"></i>GALLERY</a>
+                </li>
+                <li class="nav-item mt">
+                  <a class="nav-link fw-bold me-4 text-dark" aria-current="page" href="blog.html"><i class="fa-solid fa-square-rss  me-2"></i>BLOGS</a>
+                </li>
+                <li class="nav-item mt">
+                  <a class="nav-link me-5 fw-bold pe-5 text-success" aria-current="page" href="contact.html"><i class="fa-regular fa-address-book  me-2"></i>CONTACT
+                    US</a>
+                </li>
+      
+              </ul>
+      
+            </div>
+      
+      </div>
+      </div>`)
